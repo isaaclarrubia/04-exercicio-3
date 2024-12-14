ddf1c71c37410dc3afe626b848b42c7d561b9330
Isaac Larrubia <isaaclfpontes@gmail.com>
2024-12-13 22:48:58 -0300

    updated home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
@@ -6,6 +5,17 @@ export default function Home() {
     <main>
       <h1>Conheça as Naves do Universo de Star Wars</h1>
 
+      <p>
+        Projeto desenvolvido durante o Exercício 3 do módulo de Next.js da trilha de Frontend da OneBitCode.
+      </p>
+      <p>
+        Tecnologias utilizadas
+      </p>
+      <ul>
+        <li>Next.js</li>
+        <li>TypeScript</li>
+        <li>Vercel</li>
+      </ul>
       <p>
         <Link className="btn" href={"/spaceships"}>Ver Todas</Link>
       </p>
