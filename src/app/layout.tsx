ea79b1a8d7f2e372660b0002483a8cf007c47c95
Isaac Larrubia <isaaclfpontes@gmail.com>
2024-12-13 22:41:04 -0300

    Exercício finalizado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <header>
+          <div>Exercício 3 - Catálogo de Espaçonaves</div>
+          <nav>
+            <Link href={"/"}>Início</Link>
+            <Link href={"/spaceships"}>Espaçonaves</Link>
+            <Link href={"/categories"}>Categorias</Link>
+          </nav>
+        </header>
         {children}
       </body>
     </html>
